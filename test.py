f7319f7aa18d0b2ed74151819058de1e3dc564b9
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2024-04-27 17:24:32 -0500

    prototype code from test.py integrated into the application

--- test.py
+++ test.py
@@ -11,18 +11,7 @@ from queue import Queue
 load_dotenv()
 
 
-class StreamingHandler(BaseCallbackHandler):
-    def __init__(self, queue):
-        self.queue = queue
 
-    def on_llm_new_token(self, token, **kwargs):
-        self.queue.put(token)
-
-    def on_llm_end(self, response, **kwargs):
-        self.queue.put(None)
-
-    def on_llm_error(self, response, **kwargs):
-        self.queue.put(None)
 
 
 chat = ChatOpenAI(
@@ -33,21 +22,7 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 
 
-class StreamableChain:
-    def stream(self, input):
-        queue = Queue()
-        handler = StreamingHandler(queue)
 
-        def task():
-            self(input, callbacks=[handler])
-
-        Thread(target=task).start()
-
-        while True:
-            token = queue.get()
-            if token is None:
-                break
-            yield token
 
 
 class StreamingChain(StreamableChain, LLMChain):
